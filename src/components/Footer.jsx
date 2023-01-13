8fe075edeaa9c75714964888db69085df67295b6
merk1024 <114272520+merk1024@users.noreply.github.com>
2023-01-13 10:09:05 +0600

    Erbo1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,7 @@ function Footer() {
     <footer className="page-footer blue darken-1">
       <div className="footer-copyright blue darken-2">
         <div className="container white-text">
-          © {new Date().getFullYear()}. Игорь Степанов
-          <a className="white-text right" href="https://t.me/ia_stepanov"></a>
-          <a className="white-text right" href="https://www.linkedin.com/in/ia-stepanov/"></a>
-          <a className="white-text right" href="https://github.com/ia-stepanov"></a>
+          © {new Date().getFullYear()}
         </div>
       </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <nav className="blue darken-1">
       <div className="nav-wrapper">
-        <a href="https://ia-stepanov.github.io/react-shop/" className="brand-logo">React Shop</a>
+        <a href="#" className="brand-logo">React Shop</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
         </ul>
       </div>
