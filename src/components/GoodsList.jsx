8a37ad9a6a978f4123caf658ddd8ec1327ba4551
merk1024 <114272520+merk1024@users.noreply.github.com>
2023-01-19 13:34:39 +0600

    Erbo3

--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -1,11 +1,32 @@
 import { GoodsItem } from './GoodsItem';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function GoodsList(props) {
-  const { goods = [], addToBasket = Function.prototype } = props;
+  let {addToBasket = Function.prototype } = props;
+  const [loading, setLoading] = useState(false);
+  const [goods, changeGoods] = useState([])
+
+  const loadingData = async () => {
+    let data = []
+
+    let responce = await axios.get('https://fakestoreapi.com/products')
+
+    data = responce.data
+
+    setLoading(true)
+
+    changeGoods(data)
+  }
+
+  if (!loading)
+    loadingData()
+
+  console.log(goods)
 
   if (!goods.length) {
     return <h3>Nothing here</h3>;
-  }
+  } 
 
   return (
     <div className="goods">
